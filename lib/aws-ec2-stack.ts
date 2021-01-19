dff6eacafa7d9d0fccfd6a807b1c60931f5b23d1
Taro Matsuzawa <taro@georepublic.co.jp>
2021-01-20 04:29:08 +0900

    added vpc

--- lib/aws-ec2-stack.ts
+++ lib/aws-ec2-stack.ts
@@ -1,9 +1,38 @@
 import * as cdk from '@aws-cdk/core';
+import { CfnInternetGateway, CfnVPCGatewayAttachment, DefaultInstanceTenancy, RouterType, Subnet, Vpc } from '@aws-cdk/aws-ec2'
+
 
 export class AwsEc2Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new Vpc(this, 'ha4db-vpc', {
+      cidr: '10.0.0.0/16',
+      defaultInstanceTenancy: DefaultInstanceTenancy.DEFAULT,
+      enableDnsSupport: true,
+      enableDnsHostnames: true,
+      subnetConfiguration: []
+    })
+
+    // Public Subnet
+    const publicSubnet = new Subnet(this, 'PublicSubnet1c', {
+      availabilityZone: 'ap-northeast-1c',
+      vpcId: vpc.vpcId,
+      cidrBlock: '10.0.0.0/24'
+    })
+
+    // Internet Gateway
+    const internetGateway = new CfnInternetGateway(this, 'InternetGateway', {})
+    new CfnVPCGatewayAttachment(this, 'gateway', {
+      vpcId: vpc.vpcId,
+      internetGatewayId: internetGateway.ref
+    })
+
+    // Router
+    publicSubnet.addRoute('PublicSubnetRoute', {
+      routerType: RouterType.GATEWAY,
+      routerId: internetGateway.ref
+    })
   }
 }
--- test/aws-ec2.test.ts
+++ test/aws-ec2.test.ts
@@ -1,13 +1,14 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import '@aws-cdk/assert/jest'
 import * as cdk from '@aws-cdk/core';
 import * as AwsEc2 from '../lib/aws-ec2-stack';
 
-test('Empty Stack', () => {
+test('VPC', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new AwsEc2.AwsEc2Stack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expect(stack).toHaveResource('AWS::EC2::VPC', {
+      CidrBlock: "10.0.0.0/16",
+      Tags: [{ 'Key': 'Name', 'Value': 'MyTestStack/ha4db-vpc'}]
+    })
 });
